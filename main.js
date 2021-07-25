65e706948921bfaedc41a823c20c9e0392dc03ea
Aoi0963 <saa_okumura@cc.nara-wu.ac.jp>
2021-07-25 19:31:04 +0900

    ポートフォリオ編集

--- main.js
+++ main.js
@@ -1,30 +1,19 @@
 $(function () {
+  /* aタグのhref属性の内容の前方に#が付いている要素を取得する。 */
   $('a[href^="#"]').click(function () {
-    /* リンク先が#から始まる時 */ var speed = 400; /* スクロールにかける時間を設定 */
-    var href = $(this).attr("href"); /* hrefの内容を取得 */
-    var target = $(
-      href == "#" ? "html" : href
-    ); /* hrefが#だけだった場合、htmlに置き換えて、そうでない場合はhrefの値をtargetに格納 */
-    var position = target.offset().top; /* 飛び先のIDの上からの位置を取得 */
-    $("body,html").animate(
-      { scrollTop: position },
-      speed,
-      "swing"
-    ); /* cssのアニメーションを使ってスクロールさせる */
+    /* スクロールにかける時間を設定 */
+    var speed = 400;
+    /* $('a[href^="#"]')　で取得した内容の属性の中身を取得 */
+    var href = $(this).attr("href");
+    /* hrefが#だけだった場合、htmlに置き換えて、そうでない場合はhrefの値をtargetに格納(?はif文のショートカット) */
+    var target = $(href == "#" ? "html" : href);
+    /* 飛び先のIDの上からの位置を取得 */
+    var position = target.offset().top;
+    /* cssのアニメーションを使ってスクロールさせる
+    .animate({変化させるCSSのキーと値のマップ値} ,[ スピード] ,[ イージングの種類] ),
+    [アニメーションが完了した時に実行したい処理] )
+    */
+    $("body,html").animate({ scrollTop: position }, speed, "swing");
     return false;
   });
-});
-
-$("top-bottun").hide();
-
-$(window).scroll(function () {
-  if ($(this).scrollTop() > 100) {
-    $("top-bottun").fadeIn();
-  } else {
-    $("top-bottun").fadeOut();
-  }
-});
-
-$("top-bottun").on("click", function () {
-  $("html, body").animate({ scrollTop: 0 }, 500);
 });
